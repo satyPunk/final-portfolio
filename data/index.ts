20717fd3109c882d8a1876fed0e718b3105cef19
satyPunk <satyamhere8@gmail.com>
2025-02-22 11:34:57 +0530

    Stage-1

---
+++ data/index.ts
@@ -0,0 +1,227 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with working accross the Globe",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "AI/ML enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building working on AI/ML models and Typescript",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Want to work on a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "A Blog Post Website For Writers and Readers",
+    des: "A simple website with a interactive ui/ux for blogs built using next.js",
+    img: "/blog.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Payload Enabled Secrets App",
+    des: "A simplified Payload enabled secrets app with OAUTH and many more features",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+  //   link: "/ui.aiimg.com",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "/ui.apple.com",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Satyam was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Satyam's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Satyam is the ideal partner.",
+    name: "Anshul Singhal",
+    title: "Freelancing Client from Fiver",
+  },
+  {
+    quote:
+      "Collaborating with Satyam was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Satyam's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Satyam is the ideal partner.",
+    name: "Anshul Singhal",
+    title: "Freelancing Client from Fiver",
+  },
+  {
+    quote:
+      "Collaborating with Satyam was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Satyam's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Satyam is the ideal partner.",
+    name: "Anshul Singhal",
+    title: "Freelancing Client from Fiver",
+  },
+  {
+    quote:
+      "Collaborating with Satyam was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Satyam's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Satyam is the ideal partner.",
+    name: "Anshul Singhal",
+    title: "Freelancing Client from Fiver",
+  },
+  {
+    quote:
+      "Collaborating with Satyam was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Satyam's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Satyam is the ideal partner.",
+    name: "Anshul Singhal",
+    title: "Freelancing Client from Fiver",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "AWS & AZURE",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "OAUTH 2.0",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "Vercel",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "NeonDB",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 6,
+    name: "Kubernetes",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full-Stack Web Developer",
+    desc: "Learnt Full stack and deployed some of them.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Learning Mobile App Devolopment",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Open to Freelancing Web Dev Project",
+    desc: "Ready to make projects on full stack.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Backend , Cloud and Database",
+    desc: "I am skilled in Backend , cloud deployment and Database modelling.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link : "https://github.com/satyPunk",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://linkedin.com/in/satyam-mishra-9329a1329"
+
+  },
+  {
+    id: 3,
+    img: "/l.png",
+    link: "https://leetcode.com/u/Saty_here/",
+  },
+];
